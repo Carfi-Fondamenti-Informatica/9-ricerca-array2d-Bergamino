652fc0a19a496bdbacfd2f003314daf3c08808a2
Bergamino <91837242+Bergamino@users.noreply.github.com>
2021-12-10 17:53:46 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,8 @@
+//
+// Created by lucab on 07/12/2021.
+//
 
+#ifndef ESERCITAZIONE_9_NOMI_LIB_H
+#define ESERCITAZIONE_9_NOMI_LIB_H
+int array(char a [10][20],char b[1][20]);
+#endif //ESERCITAZIONE_9_NOMI_LIB_H
