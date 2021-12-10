78492ea5e8843af555756e51800a40e64f320d9a
Bergamino <91837242+Bergamino@users.noreply.github.com>
2021-12-10 18:29:03 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -3,7 +3,7 @@
 #include <string.h>
 using namespace std;
 
-bool array(char list[10][20],char name[20],int &i){
+bool res(char list[10][20],char name[20],int &i){
 for(i=0;i<10;i++){
     if((strcmp(list[i],name))==0){
         return true;
